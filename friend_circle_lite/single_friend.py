e2a41bf40aa5386ffa70c6224487d84186908f9e
BugX <41711995+mrxie68@users.noreply.github.com>
2026-02-27 20:05:17 +0800

    Update single_friend.py

--- friend_circle_lite/single_friend.py
+++ friend_circle_lite/single_friend.py
@@ -27,7 +27,9 @@ def check_feed(blog_url, session):
         ('feed2', '/feed.xml'),  # 2024-07-26 添加 /feed.xml内容的支持
         ('feed3', '/feed/'),
         ('feed4', '/feed.php'),  # 2025-07-22 添加 /feed.php内容的支持
-        ('index', '/index.xml')  # 2024-07-25 添加 /index.xml内容的支持
+        ('index', '/index.xml'),
+        ('feed4', '/atom.xml')
+        # 2024-07-25 添加 /index.xml内容的支持
     ]
 
     for feed_type, path in possible_feeds:
